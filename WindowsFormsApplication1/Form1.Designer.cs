7ac9cd23deb22bfe22acfed2dcad73af8f64b1fe
avk013 <avk013@gmail.com>
2016-10-19 07:06:35 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.geckoWebBrowser1 = new Gecko.GeckoWebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // geckoWebBrowser1
+            // 
+            this.geckoWebBrowser1.Location = new System.Drawing.Point(-2, 1);
+            this.geckoWebBrowser1.Name = "geckoWebBrowser1";
+            this.geckoWebBrowser1.Size = new System.Drawing.Size(676, 340);
+            this.geckoWebBrowser1.TabIndex = 0;
+            this.geckoWebBrowser1.UseHttpActivityObserver = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(268, 455);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 455);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 52);
+            this.textBox1.MaxLength = 63;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(101, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "ВАО";
+            this.textBox1.WordWrap = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(43, 455);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.YellowGreen;
+            this.button4.Location = new System.Drawing.Point(494, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 77);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "подходит";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightSalmon;
+            this.button5.Location = new System.Drawing.Point(494, 202);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(161, 75);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "лишнее";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(526, 68);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(129, 20);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "считываем из файла";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 487);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.geckoWebBrowser1);
+            this.Name = "Form1";
+            this.Text = "go_cite";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeBegin += new System.EventHandler(this.Form1_ResizeBegin);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_ClientSizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Gecko.GeckoWebBrowser geckoWebBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,200 @@
+﻿using Gecko;
+using Gecko.DOM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string file_path = @"F:\brows123\1\";
+        string[][] tab_array_in_double = null;
+        int ch = 0;
+        string regions = "garden";
+        public Form1()
+        {
+            InitializeComponent();
+            Gecko.Xpcom.Initialize(@"F:\brows123\xulrunner\bin");
+        }
+        public void WriteQuery(string query)
+        {
+            // получаем все элементы с тегом input
+            var inputsElements = this.geckoWebBrowser1.Document.GetElementsByTagName("input");
+            // получаем первый input, имя (атрибут "name") которого равно q
+            var queryElement = inputsElements.First(i => i.GetAttribute("name") == "q");
+            // "плавный" набор поискового запроса
+            Random random = new Random();
+            for (int i = 0; i < query.Length; i++)
+            {
+                queryElement.SetAttribute("value", query.Substring(0, i + 1));
+                //await TaskEx.Delay(random.Next(100, 350));
+            }
+            // получаем все элементы с тегом button
+            var buttonsElements = this.geckoWebBrowser1.Document.GetElementsByTagName("button");
+            // указываем, что началась загрузка, кликаем по первой кнопке с именем "btnG, запускаем таймер и ждем загрузки страницы
+            //this._loading = true;
+            //buttonsElements.First(i => i.GetAttribute("name") == "btnG").Click();
+            //this._loadingTimer.Start();
+            //await this.WaitForLoading();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //geckoWebBrowser1.Navigate("ya.ru");
+            //geckoWebBrowser1.
+            //WriteQuery("lalala");
+            /*  GeckoHtmlElement element = null;
+
+              var geckoDomElement = WebBrowser1.Document.DocumentElement;
+              if (geckoDomElement is GeckoHtmlElement)
+              {
+                  element = (GeckoHtmlElement)geckoDomElement;
+                  var innerHtml = element.InnerHtml;
+              }*/
+            GeckoHtmlElement element = null;
+            //string innerHtml = element.InnerHtml;
+            //var innerHtml;
+            string s="";
+            var geckoDomElement = geckoWebBrowser1.Document.DocumentElement;
+            if (geckoDomElement is GeckoHtmlElement)
+            {
+                element = (GeckoHtmlElement)geckoDomElement;
+            var    innerHtml = element.InnerHtml;
+                s=s+ innerHtml.ToString();
+
+            }
+            
+            
+            textBox1.Text = s;
+            geckoWebBrowser1.Navigate("https://www.yandex.ru/search/?text=время&lr=213&clid=9582");
+        }
+        //string[] word = { "123","asfsa","asfdfsa"};
+        //int nom = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            geckoWebBrowser1.Navigate("https://www.yandex.ru/search/?text=boom&lr=213&clid=9582&rstr=-213");
+          //  nom += 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+ 
+
+            // получаем все элементы с тегом input
+            var inputsElements = geckoWebBrowser1.Document.GetElementsByTagName("input");
+            // получаем первый input, имя (атрибут "name") которого равно q
+            //string b = "";
+            //for (int i = 0; i < inputsElements.Length; i++) b = b + inputsElements.ToString();
+            var queryElement = inputsElements.First(i => i.GetAttribute("name") == "text");
+            textBox1.Text = queryElement.GetAttribute("value") ;
+
+            var buttonsElements = geckoWebBrowser1.Document.GetElementsByTagName("button");
+            ///
+            string query = "dsfdsfdsfsdgd111";
+            for (int i = 0; i < query.Length; i++)
+            {
+                queryElement.SetAttribute("value", query.Substring(0, i + 1));
+                //await TaskEx.Delay(random.Next(100, 350));
+            }
+            ///
+
+            //queryElement.SetAttribute("value", "66666");
+            buttonsElements.First(i => i.GetAttribute("role") == "button").Click();
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+
+          
+
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            geckoWebBrowser1.Width = this.Width - 15;
+            geckoWebBrowser1.Height = this.Height - 15;
+            button6.Top = 70;
+            button4.Top = 100;
+            button5.Top = 200;
+            button6.Left=button5.Left=button4.Left = geckoWebBrowser1.Width - 180;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button4.Enabled = button5.Enabled = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text Files|*.csv";
+            openFileDialog1.Title = "фал после для обработки";
+            openFileDialog1.FileName = "1.csv";
+            openFileDialog1.InitialDirectory = Environment.SpecialFolder.Desktop.ToString();
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\1.csv";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                path = openFileDialog1.FileName;
+            
+            string[] tab0 = File.ReadAllLines(path, Encoding.Default);
+            string[] tab_array_in;
+            tab_array_in_double = new string[tab0.Length][];
+            
+            for (int i = 0; i < tab0.Length; i++)
+            {
+                tab_array_in_double[i] = new string[2];
+                if (!String.IsNullOrEmpty(tab0[i]))
+                {
+                    tab_array_in = tab0[i].Split(';');
+                    for (int j = 0; j < 2; j++)
+                    { tab_array_in_double[i][j] = tab_array_in[j]; }
+                }
+            }
+            geckoWebBrowser1.Navigate("https://www.yandex.ru/search/?text=" + tab_array_in_double[ch][1] + " " + regions + " &lr=213&clid=9582&rstr=-213");
+            ch++;
+            button4.Enabled = button5.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            geckoWebBrowser1.Navigate("https://www.yandex.ru/search/?text=" + tab_array_in_double[ch][1] + " "+regions+" &lr=213&clid=9582&rstr=-213");
+            File.AppendAllText(file_path+"2.csv", tab_array_in_double[ch - 1][0]+";"+tab_array_in_double[ch-1][1]+Environment.NewLine);
+            ch++;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            geckoWebBrowser1.Navigate("https://www.yandex.ru/search/?text=" + tab_array_in_double[ch][1] + " " + regions + " &lr=213&clid=9582&rstr=-213");
+            ch++;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            regions = textBox1.Text;
+           // textBox1.Text = regions + "+";
+        }
+    }
+}
